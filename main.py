ea914b59689826b8e619f9f3c98e286b59b3a711
Ritika <ritikakumari1302@gmail.com>
2019-02-28 00:33:13 +0530

    improved code

--- main.py
+++ main.py
@@ -3,17 +3,24 @@ from difflib import SequenceMatcher
 import multiprocessing
 import PyPDF2
 from fuzzywuzzy import fuzz 
+from tqdm import tqdm 
 
-def check_ratio(file):
-	flag = 0
-	file = os.path.join(path, file)
-	check_data = read_pdf(File)
-	database_data = read_pdf(file)
-	similarity_ratio = fuzz.ratio(check_data, database_data)
-	print(similarity_ratio)
-	if similarity_ratio > 50:               #To increase the string matching efficiency, decrease the similarity ratio 
-		print("Copied thesis from {}".format(file))
-		flag = 1
+def check_ratio(folder):
+	global flag
+	try:
+		for file in os.listdir(folder):
+			file = os.path.join(folder, file)
+			check_data = read_pdf(File)
+			database_data = read_pdf(file)
+			similarity_ratio = fuzz.ratio(check_data, database_data)
+			#token_set_ratio = fuzz.token_set_ratio(check_data, database_data)
+			#print("{} - {}".format(file,similarity_ratio))
+			if similarity_ratio > 50:               #To increase the string matching efficiency, decrease the similarity ratio 
+				print("Copied thesis from {}".format(file))
+				flag = 1
+				break
+	except:
+		pass
 	return flag
 
 
@@ -29,39 +36,68 @@ def read_pdf(file):
 	return string
 
 def thread1(patch1, return1):
-	for file in patch1:
-		flag = check_ratio(file)
+	for folder in tqdm(patch1):
+		folder = os.path.join(path,folder)
+		flag = check_ratio(folder)
 		return1[flag] = flag
 
 def thread2(patch2, return2):
-	for file in patch2:
-		flag = check_ratio(file)
+	for folder in tqdm(patch2):
+		folder = os.path.join(path,folder)
+		flag = check_ratio(folder)
 		return2[flag] = flag
 
+def thread3(patch3, return3):
+	for folder in tqdm(patch3):
+		folder = os.path.join(path,folder)
+		flag = check_ratio(folder)
+		return3[flag] = flag
+
+def thread4(patch4, return4):
+	for folder in tqdm(patch4):
+		folder = os.path.join(path,folder)
+		flag = check_ratio(folder)
+		return4[flag] = flag
+
 if __name__ == '__main__':
-	File = 'Primjer_esej.pdf'
-	path = 'Database/'
+	File = 'sample.pdf'
+	path = 'dataset/'
+	global flag
+	flag = 0
 
 	listOfFile = os.listdir(path)
 	numberOfFiles = len(os.listdir(path))
-	p1 = int(numberOfFiles/2)
-	patch1 = listOfFile[0:p1]
-	patch2 = listOfFile[p1:numberOfFiles]
 
+	partition = [int(numberOfFiles*i/4) for i in range(0,5)]
 	manager = multiprocessing.Manager()
+	#print(partition)
+
+	patch = []
+	patches = []
+	for i in partition:
+		if i != 0:
+			patch = listOfFile[j:i]
+			patches.append(patch)
+		j = i
+		#print(patch)
+	
 	return1 = manager.dict()
 	return2 = manager.dict()
+	return3 = manager.dict()
+	return4 = manager.dict()
 
-	p1 = multiprocessing.Process(target=thread1, args=(patch1, return1)) 
-	p2 = multiprocessing.Process(target=thread2, args=(patch2, return2)) 
+	p1 = multiprocessing.Process(target=thread1, args=(patches[0:1][0], return1)) 
+	p2 = multiprocessing.Process(target=thread2, args=(patches[1:2][0], return2)) 
+	p3 = multiprocessing.Process(target=thread3, args=(patches[2:3][0], return3)) 
+	p4 = multiprocessing.Process(target=thread4, args=(patches[3:4][0], return4)) 
 
-	p1.start()
-	p2.start()
+	process = [p1, p2, p3, p4]
+	for p in process:
+		p.start()
+	for p in process:
+		p.join()
 
-	p1.join()
-	p2.join()
+	#print(return1, return2, return3, return4)
 
-	if return1 == 0 and return2 == 0:
+	if return1[0] == 0 and return2[0] == 0 and return3[0] == 0 and return4[0] == 0:
 		print("Bravo! Original thesis")
-
-
